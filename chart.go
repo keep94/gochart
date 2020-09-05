281f51ff8d8515cb4319ba798969a5da116e0f20
Travis Keep <keep94@gmail.com>
2020-09-05 13:49:26 -0700

    gofmt everything.

--- chart.go
+++ chart.go
@@ -1,52 +1,52 @@
 package gochart
 
 import (
-  "fmt"
-  "io"
-  "math/big"
-  "os"
-  "strconv"
-  "strings"
+	"fmt"
+	"io"
+	"math/big"
+	"os"
+	"strconv"
+	"strings"
 
-  "github.com/keep94/gomath"
+	"github.com/keep94/gomath"
 )
 
 const (
-  kIdxOutOfRange = "idx out of range"
+	kIdxOutOfRange = "idx out of range"
 )
 
 // Ints is a sequence of integer X values.
 // Note that Ints implements the Values interface.
 type Ints struct {
-  start int64
-  inc int64
-  count int
+	start int64
+	inc   int64
+	count int
 }
 
 // NewInts returns a sequence of count integers starting at start and
 // incrementing by inc.
 func NewInts(start, inc int64, count int) *Ints {
-  return &Ints{start: start, inc: inc, count: count}
+	return &Ints{start: start, inc: inc, count: count}
 }
 
 // Apply applies f to each of these X values and returns the resulting
 // Y values.
 func (i *Ints) Apply(f func(int64) int64) Values {
-  result := make(valueSlice, i.count)
-  for j := 0; j < i.count; j++ {
-    result[j] = f(i.value(j))
-  }
-  return result
+	result := make(valueSlice, i.count)
+	for j := 0; j < i.count; j++ {
+		result[j] = f(i.value(j))
+	}
+	return result
 }
 
 // ApplyBigInt applies f to each of these X values and returns the resulting
 // Y values. f must store the result in result and return result.
 func (i *Ints) ApplyBigInt(f func(x int64, result *big.Int) *big.Int) Values {
-  result := make(valueSlice, i.count)
-  for j := 0; j < i.count; j++ {
-    result[j] = f(i.value(j), new(big.Int))
-  }
-  return result
+	result := make(valueSlice, i.count)
+	for j := 0; j < i.count; j++ {
+		result[j] = f(i.value(j), new(big.Int))
+	}
+	return result
 }
 
 // ApplyBigIntStream uses stream to return the resulting Y values.
@@ -57,7 +57,7 @@ func (i *Ints) ApplyBigInt(f func(x int64, result *big.Int) *big.Int) Values {
 // i.ApplyBigIntStream(stream) is the same as
 // i.ApplyBigInt(gomath.NewNthBigInt(stream).Nth)
 func (i *Ints) ApplyBigIntStream(stream gomath.BigIntStream) Values {
-  return i.ApplyBigInt(gomath.NewNthBigInt(stream).Nth)
+	return i.ApplyBigInt(gomath.NewNthBigInt(stream).Nth)
 }
 
 // ApplyStream uses stream to return the resulting Y values.
@@ -67,51 +67,51 @@ func (i *Ints) ApplyBigIntStream(stream gomath.BigIntStream) Values {
 // or else ApplyStream panics. If stream runs out of values, the resulting Y
 // value is always 0.
 func (i *Ints) ApplyStream(stream gomath.IntStream) Values {
-  nth := gomath.NewNthInt(stream)
-  return i.Apply(
-      func(x int64) int64 {
-        y, _ := nth.SafeNth(x)
-        return y
-      })
+	nth := gomath.NewNthInt(stream)
+	return i.Apply(
+		func(x int64) int64 {
+			y, _ := nth.SafeNth(x)
+			return y
+		})
 }
 
 func (i *Ints) Value(idx int) interface{} {
-  if idx<0 || idx>=i.count {
-    panic(kIdxOutOfRange)
-  }
-  return i.value(idx)
+	if idx < 0 || idx >= i.count {
+		panic(kIdxOutOfRange)
+	}
+	return i.value(idx)
 }
 
 func (i *Ints) Len() int {
-  return i.count
+	return i.count
 }
 
 func (i *Ints) value(idx int) int64 {
-  return i.start + int64(idx)*i.inc
+	return i.start + int64(idx)*i.inc
 }
 
 // Floats is a sequence of floating point X values.
 // Note that Floats implements the Values interface.
 type Floats struct {
-  start float64
-  inc float64
-  count int
+	start float64
+	inc   float64
+	count int
 }
 
 // NewFloats returns a sequence of count floats starting at start and
 // incrementing by inc.
 func NewFloats(start, inc float64, count int) *Floats {
-  return &Floats{start: start, inc: inc, count: count}
+	return &Floats{start: start, inc: inc, count: count}
 }
 
 // Apply applies fn to each of these X values and returns the resulting
 // Y values.
 func (f *Floats) Apply(fn func(float64) float64) Values {
-  result := make(valueSlice, f.count)
-  for i := 0; i < f.count; i++ {
-    result[i] = fn(f.value(i))
-  }
-  return result
+	result := make(valueSlice, f.count)
+	for i := 0; i < f.count; i++ {
+		result[i] = fn(f.value(i))
+	}
+	return result
 }
 
 // ApplyInv applies the inverse of fn to each of these X values and returns the
@@ -119,38 +119,38 @@ func (f *Floats) Apply(fn func(float64) float64) Values {
 // lower and upper. fn must be monotone increasing or decreasing between
 // lower and upper.
 func (f *Floats) ApplyInv(
-    fn func(float64) float64, lower, upper float64) Values {
-  result := make(valueSlice, f.count)
-  for i := 0; i < f.count; i++ {
-    result[i] = gomath.Inverse(fn, f.value(i), lower, upper)
-  }
-  return result
+	fn func(float64) float64, lower, upper float64) Values {
+	result := make(valueSlice, f.count)
+	for i := 0; i < f.count; i++ {
+		result[i] = gomath.Inverse(fn, f.value(i), lower, upper)
+	}
+	return result
 }
 
 func (f *Floats) Value(idx int) interface{} {
-  if idx<0 || idx>=f.count {
-    panic(kIdxOutOfRange)
-  }
-  return f.value(idx)
+	if idx < 0 || idx >= f.count {
+		panic(kIdxOutOfRange)
+	}
+	return f.value(idx)
 }
 
 func (f *Floats) Len() int {
-  return f.count
+	return f.count
 }
 
 func (f *Floats) value(idx int) float64 {
-  return f.start + float64(idx)*f.inc
+	return f.start + float64(idx)*f.inc
 }
 
 // Interface Values represents a sequence values for either the X or Y column
 // of a chart.
 type Values interface {
 
-  // Returns the 0-based idx value in this sequence of values
-  Value(idx int) interface{}
+	// Returns the 0-based idx value in this sequence of values
+	Value(idx int) interface{}
 
-  // Returns the number of values
-  Len() int
+	// Returns the number of values
+	Len() int
 }
 
 // Option represents an option for creating a chart.
@@ -158,16 +158,16 @@ type Option option
 
 // XFormat sets the format string for formatting X values, default is "%v"
 func XFormat(fmtStr string) Option {
-  return func(s *settingsType) {
-    s.xFormat = fmtStr
-  }
+	return func(s *settingsType) {
+		s.xFormat = fmtStr
+	}
 }
 
 // YFormat sets the format string for formatting Y values, default is "%v"
 func YFormat(fmtStr string) Option {
-  return func(s *settingsType) {
-    s.yFormat = fmtStr
-  }
+	return func(s *settingsType) {
+		s.yFormat = fmtStr
+	}
 }
 
 // NumRows sets the number of rows in the chart. The default number of rows
@@ -175,9 +175,9 @@ func YFormat(fmtStr string) Option {
 // number of columns. If neither numRows or numCols are set, numRows
 // defaults to the number of values and numCols defaults to 1.
 func NumRows(count int) Option {
-  return func(s *settingsType) {
-    s.numRows = count
-  }
+	return func(s *settingsType) {
+		s.numRows = count
+	}
 }
 
 // NumCols sets the number of columns in the chart. The default number of
@@ -185,170 +185,170 @@ func NumRows(count int) Option {
 // given the number of rows. If neither numRows or numCols are set, numCols
 // defaults to 1 and numRows defaults to the number of values.
 func NumCols(count int) Option {
-  return func(s *settingsType) {
-    s.numCols = count
-  }
+	return func(s *settingsType) {
+		s.numCols = count
+	}
 }
 
 // Chart represents a chart of X and Y values.
 type Chart struct {
-  header string
-  xyFormat string
-  numRows int
-  numCols int
-  xyValues xyValuesType
+	header   string
+	xyFormat string
+	numRows  int
+	numCols  int
+	xyValues xyValuesType
 }
 
 // NewChart creates a new chart. xs are the X values and ys are the Y values.
 // The number of X and Y values must be the same or else NewChart panics.
 // options are the options for creating the chart.
 func NewChart(xs, ys Values, options ...Option) *Chart {
-  if xs.Len() != ys.Len() {
-    panic("xs and ys must have same length")
-  }
-  settings := &settingsType{xFormat: "%v", yFormat: "%v"}
-  settings.applyOptions(options)
-  settings.computeDimensions(xs.Len())
-  xyValues := createXYValues(xs, ys, settings.xFormat, settings.yFormat)
-  xwidth, ywidth := xyValues.widths()
-  return &Chart{
-      header: createHeader(xwidth, ywidth, settings.numCols),
-      xyFormat: createXYFormat(xwidth, ywidth),
-      numRows: settings.numRows,
-      numCols: settings.numCols,
-      xyValues: xyValues}
+	if xs.Len() != ys.Len() {
+		panic("xs and ys must have same length")
+	}
+	settings := &settingsType{xFormat: "%v", yFormat: "%v"}
+	settings.applyOptions(options)
+	settings.computeDimensions(xs.Len())
+	xyValues := createXYValues(xs, ys, settings.xFormat, settings.yFormat)
+	xwidth, ywidth := xyValues.widths()
+	return &Chart{
+		header:   createHeader(xwidth, ywidth, settings.numCols),
+		xyFormat: createXYFormat(xwidth, ywidth),
+		numRows:  settings.numRows,
+		numCols:  settings.numCols,
+		xyValues: xyValues}
 }
 
 func createHeader(xwidth, ywidth, numCols int) string {
-  piece := "+" + strings.Repeat("-", xwidth) + "+" + strings.Repeat("-", ywidth)
-  return fmt.Sprintf("%s+", strings.Repeat(piece, numCols))
+	piece := "+" + strings.Repeat("-", xwidth) + "+" + strings.Repeat("-", ywidth)
+	return fmt.Sprintf("%s+", strings.Repeat(piece, numCols))
 }
 
 func createXYFormat(xwidth, ywidth int) string {
-  return "|%" + strconv.Itoa(xwidth) + "s|%" + strconv.Itoa(ywidth) + "s"
+	return "|%" + strconv.Itoa(xwidth) + "s|%" + strconv.Itoa(ywidth) + "s"
 }
 
 // WriteTo writes the chart to writer w. If w is nil, WriteTo writes the
 // chart to stdout. WriteTo returns the number of bytes written and any
 // error encountered.
 func (c *Chart) WriteTo(w io.Writer) (n int, err error) {
-  if w == nil {
-    w = os.Stdout
-  }
-  var nn int
-  nn, err = fmt.Fprintln(w, c.header)
-  n += nn
-  if err != nil {
-    return
-  }
-  for i := 0; i < c.numRows; i++ {
-    for j := 0; j < c.numCols; j++ {
-      xyValue := c.xy(i, j)
-      nn, err = fmt.Fprintf(w, c.xyFormat, xyValue.x, xyValue.y)
-      n += nn
-      if err != nil {
-        return
-      }
-    }
-    nn, err = fmt.Fprintln(w, "|")
-    n += nn
-    if err != nil {
-      return
-    }
-  }
-  nn, err = fmt.Fprintln(w, c.header)
-  n += nn
-  return
+	if w == nil {
+		w = os.Stdout
+	}
+	var nn int
+	nn, err = fmt.Fprintln(w, c.header)
+	n += nn
+	if err != nil {
+		return
+	}
+	for i := 0; i < c.numRows; i++ {
+		for j := 0; j < c.numCols; j++ {
+			xyValue := c.xy(i, j)
+			nn, err = fmt.Fprintf(w, c.xyFormat, xyValue.x, xyValue.y)
+			n += nn
+			if err != nil {
+				return
+			}
+		}
+		nn, err = fmt.Fprintln(w, "|")
+		n += nn
+		if err != nil {
+			return
+		}
+	}
+	nn, err = fmt.Fprintln(w, c.header)
+	n += nn
+	return
 }
 
 // NumRows returns the number of rows in this chart.
 func (c *Chart) NumRows() int {
-  return c.numRows
+	return c.numRows
 }
 
 // NumCols returns the number of columns in this chart.
 func (c *Chart) NumCols() int {
-  return c.numCols
+	return c.numCols
 }
 
 func (c *Chart) xy(row, col int) xyValueType {
-  idx := row + c.numRows*col
-  var result xyValueType
-  if idx < len(c.xyValues) {
-    result = c.xyValues[idx]
-  }
-  return result
+	idx := row + c.numRows*col
+	var result xyValueType
+	if idx < len(c.xyValues) {
+		result = c.xyValues[idx]
+	}
+	return result
 }
 
 type xyValueType struct {
-  x string
-  y string
+	x string
+	y string
 }
 
 type xyValuesType []xyValueType
 
 func createXYValues(
-    xs, ys Values, xformat, yformat string) xyValuesType {
-  result := make(xyValuesType, xs.Len())
-  for i := 0; i < xs.Len(); i++ {
-    result[i].x = fmt.Sprintf(xformat, xs.Value(i))
-    result[i].y = fmt.Sprintf(yformat, ys.Value(i))
-  }
-  return result
+	xs, ys Values, xformat, yformat string) xyValuesType {
+	result := make(xyValuesType, xs.Len())
+	for i := 0; i < xs.Len(); i++ {
+		result[i].x = fmt.Sprintf(xformat, xs.Value(i))
+		result[i].y = fmt.Sprintf(yformat, ys.Value(i))
+	}
+	return result
 }
 
 func (xy xyValuesType) widths() (xwidth int, ywidth int) {
-  for i := 0; i < len(xy); i++ {
-    if len(xy[i].x) > xwidth {
-      xwidth = len(xy[i].x)
-    }
-    if len(xy[i].y) > ywidth {
-      ywidth = len(xy[i].y)
-    }
-  }
-  return
+	for i := 0; i < len(xy); i++ {
+		if len(xy[i].x) > xwidth {
+			xwidth = len(xy[i].x)
+		}
+		if len(xy[i].y) > ywidth {
+			ywidth = len(xy[i].y)
+		}
+	}
+	return
 }
 
 type option func(s *settingsType)
 
 type settingsType struct {
-  xFormat string
-  yFormat string
-  numRows int
-  numCols int
+	xFormat string
+	yFormat string
+	numRows int
+	numCols int
 }
 
 func (s *settingsType) applyOptions(options []Option) {
-  for _, option := range options {
-    option(s)
-  }
+	for _, option := range options {
+		option(s)
+	}
 }
 
 func (s *settingsType) computeDimensions(count int) {
-  if s.numRows <= 0 && s.numCols <= 0 {
-    s.numRows = count
-    s.numCols = 1
-    return
-  }
-  if s.numRows <= 0 {
-    s.numRows = (count + s.numCols - 1) / s.numCols
-    return
-  }
-  if s.numCols <= 0 {
-    s.numCols = (count + s.numRows - 1) / s.numRows
-    return
-  }
+	if s.numRows <= 0 && s.numCols <= 0 {
+		s.numRows = count
+		s.numCols = 1
+		return
+	}
+	if s.numRows <= 0 {
+		s.numRows = (count + s.numCols - 1) / s.numCols
+		return
+	}
+	if s.numCols <= 0 {
+		s.numCols = (count + s.numRows - 1) / s.numRows
+		return
+	}
 }
 
 type valueSlice []interface{}
 
 func (v valueSlice) Value(idx int) interface{} {
-  if idx < 0 || idx >= len(v) {
-    panic(kIdxOutOfRange)
-  }
-  return v[idx]
+	if idx < 0 || idx >= len(v) {
+		panic(kIdxOutOfRange)
+	}
+	return v[idx]
 }
 
 func (v valueSlice) Len() int {
-  return len(v)
+	return len(v)
 }
--- chart_test.go
+++ chart_test.go
@@ -1,270 +1,270 @@
 package gochart_test
 
 import (
-  "math"
-  "math/big"
-  "testing"
+	"math"
+	"math/big"
+	"testing"
 
-  "github.com/keep94/gochart"
-  "github.com/keep94/gomath"
+	"github.com/keep94/gochart"
+	"github.com/keep94/gomath"
 )
 
 func TestApply(t *testing.T) {
-  xs := gochart.NewInts(5, 2, 3)
-  assertValuesEqual(t, xs, int64(5), int64(7), int64(9))
-  ys := xs.Apply(func(x int64) int64 { return 3*x })
-  assertValuesEqual(t, ys, int64(15), int64(21), int64(27))
+	xs := gochart.NewInts(5, 2, 3)
+	assertValuesEqual(t, xs, int64(5), int64(7), int64(9))
+	ys := xs.Apply(func(x int64) int64 { return 3 * x })
+	assertValuesEqual(t, ys, int64(15), int64(21), int64(27))
 }
 
 func TestApplyBigInt(t *testing.T) {
-  xs := gochart.NewInts(10, 1, 4)
-  ys := xs.ApplyBigInt(
-      func(x int64, result *big.Int) *big.Int {
-        bx := big.NewInt(x)
-        return result.Mul(bx, bx)
-      })
-  assertBigValuesEqual(t, ys, 100, 121, 144, 169)
+	xs := gochart.NewInts(10, 1, 4)
+	ys := xs.ApplyBigInt(
+		func(x int64, result *big.Int) *big.Int {
+			bx := big.NewInt(x)
+			return result.Mul(bx, bx)
+		})
+	assertBigValuesEqual(t, ys, 100, 121, 144, 169)
 }
 
 func TestApplyBigIntStream(t *testing.T) {
-  xs := gochart.NewInts(3, 3, 5)
-  ys := xs.ApplyBigIntStream(upBy2())
-  assertBigValuesEqual(t, ys, 6, 12, 18, 24, 30)
+	xs := gochart.NewInts(3, 3, 5)
+	ys := xs.ApplyBigIntStream(upBy2())
+	assertBigValuesEqual(t, ys, 6, 12, 18, 24, 30)
 }
 
 func TestApplyBigIntStreamSame(t *testing.T) {
-  xs := gochart.NewInts(3, 0, 5)
-  assertPanic(t, func() {
-    xs.ApplyBigIntStream(upBy2())
-  })
+	xs := gochart.NewInts(3, 0, 5)
+	assertPanic(t, func() {
+		xs.ApplyBigIntStream(upBy2())
+	})
 }
 
 func TestApplyBigIntStreamLess1(t *testing.T) {
-  xs := gochart.NewInts(0, 3, 5)
-  assertPanic(t, func() {
-    xs.ApplyBigIntStream(upBy2())
-  })
+	xs := gochart.NewInts(0, 3, 5)
+	assertPanic(t, func() {
+		xs.ApplyBigIntStream(upBy2())
+	})
 }
 
 func TestApplyBigIntStreamDown(t *testing.T) {
-  xs := gochart.NewInts(15, -3, 5)
-  assertPanic(t, func() {
-    xs.ApplyBigIntStream(upBy2())
-  })
+	xs := gochart.NewInts(15, -3, 5)
+	assertPanic(t, func() {
+		xs.ApplyBigIntStream(upBy2())
+	})
 }
 
 func TestApplyStream(t *testing.T) {
-  xs := gochart.NewInts(3, 3, 6)
-  ys := xs.ApplyStream(to30By2())
-  assertValuesEqual(
-      t, ys, int64(6), int64(12), int64(18), int64(24), int64(30), int64(0))
+	xs := gochart.NewInts(3, 3, 6)
+	ys := xs.ApplyStream(to30By2())
+	assertValuesEqual(
+		t, ys, int64(6), int64(12), int64(18), int64(24), int64(30), int64(0))
 }
 
 func TestApplyStreamSame(t *testing.T) {
-  xs := gochart.NewInts(3, 0, 5)
-  assertPanic(t, func() {
-    xs.ApplyStream(to30By2())
-  })
+	xs := gochart.NewInts(3, 0, 5)
+	assertPanic(t, func() {
+		xs.ApplyStream(to30By2())
+	})
 }
 
 func TestApplyStreamLess1(t *testing.T) {
-  xs := gochart.NewInts(0, 3, 5)
-  assertPanic(t, func() {
-    xs.ApplyStream(to30By2())
-  })
+	xs := gochart.NewInts(0, 3, 5)
+	assertPanic(t, func() {
+		xs.ApplyStream(to30By2())
+	})
 }
 
 func TestApplyStreamDown(t *testing.T) {
-  xs := gochart.NewInts(15, -3, 5)
-  assertPanic(t, func() {
-    xs.ApplyStream(to30By2())
-  })
+	xs := gochart.NewInts(15, -3, 5)
+	assertPanic(t, func() {
+		xs.ApplyStream(to30By2())
+	})
 }
 
 func TestApplyFloat(t *testing.T) {
-  xs := gochart.NewFloats(1.0, 2.0, 4)
-  ys := xs.Apply(func(x float64) float64 {
-          return x*x
-        })
-  assertValuesEqual(t, xs, 1.0, 3.0, 5.0, 7.0)
-  assertValuesEqual(t, ys, 1.0, 9.0, 25.0, 49.0)
+	xs := gochart.NewFloats(1.0, 2.0, 4)
+	ys := xs.Apply(func(x float64) float64 {
+		return x * x
+	})
+	assertValuesEqual(t, xs, 1.0, 3.0, 5.0, 7.0)
+	assertValuesEqual(t, ys, 1.0, 9.0, 25.0, 49.0)
 }
 
 func TestApplyInvFloat(t *testing.T) {
-  xs := gochart.NewFloats(0.0, 1.0, 6)
-  ys := xs.ApplyInv(
-      func(x float64) float64 {
-        return x*x
-      },
-      1,
-      2)
-  if ys.Len() != 6 {
-    t.Fatal("Expected length of 4")
-  }
-  if ys.Value(0).(float64) != 1.0 {
-    t.Error("Expected 0 to give value of 1")
-  }
-  if ys.Value(1).(float64) != 1.0 {
-    t.Error("Expected 1 to give value of 1")
-  }
-  assertCloseTo(t, 1.4142, ys.Value(2).(float64))
-  assertCloseTo(t, 1.7321, ys.Value(3).(float64))
-  if ys.Value(4).(float64) != 2.0 {
-    t.Error("Expected 4 to give value of 2")
-  }
-  if ys.Value(5).(float64) != 2.0 {
-    t.Error("Expected 5 to give value of 2")
-  }
+	xs := gochart.NewFloats(0.0, 1.0, 6)
+	ys := xs.ApplyInv(
+		func(x float64) float64 {
+			return x * x
+		},
+		1,
+		2)
+	if ys.Len() != 6 {
+		t.Fatal("Expected length of 4")
+	}
+	if ys.Value(0).(float64) != 1.0 {
+		t.Error("Expected 0 to give value of 1")
+	}
+	if ys.Value(1).(float64) != 1.0 {
+		t.Error("Expected 1 to give value of 1")
+	}
+	assertCloseTo(t, 1.4142, ys.Value(2).(float64))
+	assertCloseTo(t, 1.7321, ys.Value(3).(float64))
+	if ys.Value(4).(float64) != 2.0 {
+		t.Error("Expected 4 to give value of 2")
+	}
+	if ys.Value(5).(float64) != 2.0 {
+		t.Error("Expected 5 to give value of 2")
+	}
 }
 
 func TestApplyInvFloatReverse(t *testing.T) {
-  xs := gochart.NewFloats(0.0, 1.0, 4)
-  ys := xs.ApplyInv(
-      func(x float64) float64 {
-        return 4.0 - x*x
-      },
-      0,
-      2)
-  if ys.Len() != 4 {
-    t.Fatal("Expected length of 4")
-  }
-  if ys.Value(0).(float64) != 2.0 {
-    t.Error("Expected value of 2")
-  }
-  assertCloseTo(t, 1.7321, ys.Value(1).(float64))
-  assertCloseTo(t, 1.4142, ys.Value(2).(float64))
-  if ys.Value(3).(float64) != 1.0 {
-    t.Error("Expected value of 1")
-  }
+	xs := gochart.NewFloats(0.0, 1.0, 4)
+	ys := xs.ApplyInv(
+		func(x float64) float64 {
+			return 4.0 - x*x
+		},
+		0,
+		2)
+	if ys.Len() != 4 {
+		t.Fatal("Expected length of 4")
+	}
+	if ys.Value(0).(float64) != 2.0 {
+		t.Error("Expected value of 2")
+	}
+	assertCloseTo(t, 1.7321, ys.Value(1).(float64))
+	assertCloseTo(t, 1.4142, ys.Value(2).(float64))
+	if ys.Value(3).(float64) != 1.0 {
+		t.Error("Expected value of 1")
+	}
 }
 
 func TestChartDimensions(t *testing.T) {
-  xs := gochart.NewInts(1, 1, 100)
-  chart := gochart.NewChart(xs, xs)
-  assertEqual(t, 100, chart.NumRows())
-  assertEqual(t, 1, chart.NumCols())
-  chart = gochart.NewChart(xs, xs, gochart.NumRows(33))
-  assertEqual(t, 33, chart.NumRows())
-  assertEqual(t, 4, chart.NumCols())
-  chart = gochart.NewChart(xs, xs, gochart.NumCols(3))
-  assertEqual(t, 34, chart.NumRows())
-  assertEqual(t, 3, chart.NumCols())
-  chart = gochart.NewChart(xs, xs, gochart.NumCols(5), gochart.NumRows(25))
-  assertEqual(t, 25, chart.NumRows())
-  assertEqual(t, 5, chart.NumCols())
+	xs := gochart.NewInts(1, 1, 100)
+	chart := gochart.NewChart(xs, xs)
+	assertEqual(t, 100, chart.NumRows())
+	assertEqual(t, 1, chart.NumCols())
+	chart = gochart.NewChart(xs, xs, gochart.NumRows(33))
+	assertEqual(t, 33, chart.NumRows())
+	assertEqual(t, 4, chart.NumCols())
+	chart = gochart.NewChart(xs, xs, gochart.NumCols(3))
+	assertEqual(t, 34, chart.NumRows())
+	assertEqual(t, 3, chart.NumCols())
+	chart = gochart.NewChart(xs, xs, gochart.NumCols(5), gochart.NumRows(25))
+	assertEqual(t, 25, chart.NumRows())
+	assertEqual(t, 5, chart.NumCols())
 }
 
 func TestNewChartPanic(t *testing.T) {
-  xs := gochart.NewInts(1, 1, 10)
-  ys := gochart.NewInts(1, 1, 9)
-  assertPanic(
-      t,
-      func() {
-        gochart.NewChart(xs, ys)
-      },
-  )
+	xs := gochart.NewInts(1, 1, 10)
+	ys := gochart.NewInts(1, 1, 9)
+	assertPanic(
+		t,
+		func() {
+			gochart.NewChart(xs, ys)
+		},
+	)
 }
 
 func TestFloatsPanic(t *testing.T) {
-  xs := gochart.NewFloats(1.0, 1.0, 10)
-  assertPanic(t, func() { xs.Value(10) })
-  assertPanic(t, func() { xs.Value(-1) })
+	xs := gochart.NewFloats(1.0, 1.0, 10)
+	assertPanic(t, func() { xs.Value(10) })
+	assertPanic(t, func() { xs.Value(-1) })
 }
 
 func TestIntsPanic(t *testing.T) {
-  xs := gochart.NewInts(1, 1, 10)
-  assertPanic(t, func() { xs.Value(10) })
-  assertPanic(t, func() { xs.Value(-1) })
+	xs := gochart.NewInts(1, 1, 10)
+	assertPanic(t, func() { xs.Value(10) })
+	assertPanic(t, func() { xs.Value(-1) })
 }
 
 func TestValuesPanic(t *testing.T) {
-  xs := gochart.NewFloats(1.0, 1.0, 10)
-  ys := xs.Apply(math.Sqrt)
-  assertPanic(t, func() { ys.Value(10) })
-  assertPanic(t, func() { ys.Value(-1) })
+	xs := gochart.NewFloats(1.0, 1.0, 10)
+	ys := xs.Apply(math.Sqrt)
+	assertPanic(t, func() { ys.Value(10) })
+	assertPanic(t, func() { ys.Value(-1) })
 }
 
 func assertValuesEqual(
-    t *testing.T, ys gochart.Values, expectedValues ...interface{}) {
-  t.Helper()
-  if ys.Len() != len(expectedValues) {
-    t.Fatalf("Expected %v values, but got %v", len(expectedValues), ys.Len())
-  }
-  for i := 0; i < ys.Len(); i++ {
-    if ys.Value(i) != expectedValues[i] {
-      t.Errorf("Expected %v, got %v", expectedValues[i], ys.Value(i))
-    }
-  }
+	t *testing.T, ys gochart.Values, expectedValues ...interface{}) {
+	t.Helper()
+	if ys.Len() != len(expectedValues) {
+		t.Fatalf("Expected %v values, but got %v", len(expectedValues), ys.Len())
+	}
+	for i := 0; i < ys.Len(); i++ {
+		if ys.Value(i) != expectedValues[i] {
+			t.Errorf("Expected %v, got %v", expectedValues[i], ys.Value(i))
+		}
+	}
 }
 
 func assertEqual(
-    t *testing.T, expected, actual interface{}) {
-  t.Helper()
-  if expected != actual {
-    t.Errorf("Expected %v, got %v", expected, actual)
-  }
+	t *testing.T, expected, actual interface{}) {
+	t.Helper()
+	if expected != actual {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	}
 }
 
 func assertBigValuesEqual(
-    t *testing.T, ys gochart.Values, expectedValues ...int64) {
-  t.Helper()
-  if ys.Len() != len(expectedValues) {
-    t.Fatalf("Expected %v values, but got %v", len(expectedValues), ys.Len())
-  }
-  for i := 0; i < ys.Len(); i++ {
-    if ys.Value(i).(*big.Int).Cmp(big.NewInt(expectedValues[i])) != 0 {
-      t.Errorf("Expected %v, got %v", expectedValues[i], ys.Value(i))
-    }
-  }
+	t *testing.T, ys gochart.Values, expectedValues ...int64) {
+	t.Helper()
+	if ys.Len() != len(expectedValues) {
+		t.Fatalf("Expected %v values, but got %v", len(expectedValues), ys.Len())
+	}
+	for i := 0; i < ys.Len(); i++ {
+		if ys.Value(i).(*big.Int).Cmp(big.NewInt(expectedValues[i])) != 0 {
+			t.Errorf("Expected %v, got %v", expectedValues[i], ys.Value(i))
+		}
+	}
 }
 
 type linearBigIntStream struct {
-  start *big.Int
-  incr *big.Int
+	start *big.Int
+	incr  *big.Int
 }
 
 func (s *linearBigIntStream) Next(value *big.Int) *big.Int {
-  value.Set(s.start)
-  s.start.Add(s.start, s.incr)
-  return value
+	value.Set(s.start)
+	s.start.Add(s.start, s.incr)
+	return value
 }
 
 func upBy2() gomath.BigIntStream {
-  return &linearBigIntStream{start: big.NewInt(2), incr: big.NewInt(2)}
+	return &linearBigIntStream{start: big.NewInt(2), incr: big.NewInt(2)}
 }
 
 type linearIntStream struct {
-  start int64
-  incr int64
-  max int64
+	start int64
+	incr  int64
+	max   int64
 }
 
 func (s *linearIntStream) Next() (result int64, ok bool) {
-  if s.start > s.max {
-    return
-  }
-  result = s.start
-  ok = true
-  s.start += s.incr
-  return
+	if s.start > s.max {
+		return
+	}
+	result = s.start
+	ok = true
+	s.start += s.incr
+	return
 }
 
 func to30By2() gomath.IntStream {
-  return &linearIntStream{start: 2, incr: 2, max: 30}
+	return &linearIntStream{start: 2, incr: 2, max: 30}
 }
 
 func assertCloseTo(t *testing.T, expected float64, actual float64) {
-  t.Helper()
-  if math.Abs((expected - actual) / expected) > 0.0001 {
-    t.Errorf("Expected %v, got %v", expected, actual)
-  }
+	t.Helper()
+	if math.Abs((expected-actual)/expected) > 0.0001 {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	}
 }
 
 func assertPanic(t *testing.T, f func()) {
-  t.Helper()
-  defer func() {
-    recover()
-  }()
-  f()
-  t.Error("Expected panic")
+	t.Helper()
+	defer func() {
+		recover()
+	}()
+	f()
+	t.Error("Expected panic")
 }
